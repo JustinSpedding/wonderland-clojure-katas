fcfc844963aeec267e15f7554116ea656c0a5114
Justin Spedding <justinspedding@users.noreply.github.com>
2016-08-16 20:22:42 -0400

    Added solutions to alphabet cipher and wonderland number

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,31 @@
 (ns alphabet-cipher.coder)
 
-(defn encode [keyword message]
-  "encodeme")
+(def ^:private char-offset (int \a))
 
-(defn decode [keyword message]
-  "decodeme")
+(defn- char-to-num [char-in] (- (int char-in) char-offset))
+
+(defn- num-to-char [num-in] (char (+ num-in char-offset)))
+
+(defn- add-mod-26 [& nums] (mod (apply + nums) 26))
+
+(defn- sub-mod-26 [& nums] (mod (apply - nums) 26))
+
+(defn- operate-on-ints-of-chars [key-word message function]
+  (let [message-nums (map char-to-num message)
+        key-nums (cycle (map char-to-num key-word))
+        output-chars (map (comp num-to-char function) message-nums key-nums)]
+    (apply str output-chars)))
+
+(defn encode [key-word message]
+  (operate-on-ints-of-chars key-word message add-mod-26))
+
+(defn decode [key-word message]
+  (operate-on-ints-of-chars key-word message sub-mod-26))
 
 (defn decipher [cipher message]
-  "decypherme")
-
+  (let [repeated-keyword (operate-on-ints-of-chars message cipher sub-mod-26)]
+    (loop [keyword-size 1]
+      (let [key-word (take keyword-size repeated-keyword)]
+        (if (every? true? (map = repeated-keyword (cycle key-word)))
+          (apply str key-word)
+          (recur (inc keyword-size)))))))
--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,18 @@
 (ns wonderland-number.finder)
 
+(defn- num-to-set-of-digits [n]
+  (set (str n)))
+
+(defn- all-have-same-digits? [& nums]
+  (apply = (map num-to-set-of-digits nums)))
+
+(def ^:private multipliers-to-test (range 1 7))
+
+(defn- num-to-set-of-multiples [wondernum]
+  (map #(* wondernum %) multipliers-to-test))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (loop [wondernum 100000]
+    (if (apply all-have-same-digits? (num-to-set-of-multiples wondernum))
+      wondernum
+      (recur (inc wondernum)))))
