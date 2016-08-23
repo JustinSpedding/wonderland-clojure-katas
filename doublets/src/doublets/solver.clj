de277fd47e809112f52eae749bb817b2347963a4
Justin Spedding <justinspedding@users.noreply.github.com>
2016-08-22 20:26:56 -0400

    Added solution to doublets kata

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -2,10 +2,30 @@
   (:require [clojure.java.io :as io]
             [clojure.edn :as edn]))
 
-(def words (-> "words.edn"
-               (io/resource)
-               (slurp)
-               (read-string)))
+(def ^:private words (-> "words.edn"
+                       (io/resource)
+                       (slurp)
+                       (read-string)))
+
+(defn- number-of-different-chars [word1 word2]
+  (count (filter false? (map = word1 word2))))
+
+(defn- potential-next-word? [word next-word already-used-words]
+  (and (not (contains? already-used-words next-word))
+       (= (count word) (count next-word))
+       (= 1 (number-of-different-chars word next-word))))
+
+(defn- next-words-to-check [word goal already-used-words]
+  (->> words
+       (filter #(potential-next-word? word % already-used-words))
+       (sort-by #(number-of-different-chars goal %))))
+
+(defn- reach-goal-from-word [word goal already-used-words]
+  (if (= word goal)
+    (list goal)
+    (if-let [next-words (some #(reach-goal-from-word % goal (conj already-used-words %)) (next-words-to-check word goal already-used-words))]
+      (conj next-words word))))
 
 (defn doublets [word1 word2]
-  "make me work")
+  (or (reach-goal-from-word word1 word2 (set [word1]))
+      (list)))
