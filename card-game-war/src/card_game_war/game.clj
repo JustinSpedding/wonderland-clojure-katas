5037036ebda400d74770fcae9d234c1b3217d581
Justin Spedding <justinspedding@users.noreply.github.com>
2016-08-24 20:23:55 -0400

    Added solution to card-game-war kata

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,13 +1,29 @@
 (ns card-game-war.game)
 
-;; feel free to use these cards or use your own data structure
 (def suits [:spade :club :diamond :heart])
+
 (def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
+
 (def cards
-  (for [suit suits
-        rank ranks]
-    [suit rank]))
+  (for [rank ranks
+        suit suits]
+    {:suit suit, :rank rank}))
 
-(defn play-round [player1-card player2-card])
+(def card-order (apply hash-map (interleave cards (range))))
 
-(defn play-game [player1-cards player2-cards])
+(defn play-round [player1-card player2-card]
+  (if (> (card-order player1-card) (card-order player2-card)) 1 2))
+
+(defn play-game [player1-cards player2-cards]
+  (loop [player1-deck (into (clojure.lang.PersistentQueue/EMPTY) player1-cards)
+         player2-deck (into (clojure.lang.PersistentQueue/EMPTY) player2-cards)]
+    (cond (empty? player1-deck) 2
+          (empty? player2-deck) 1
+          :else (let [player1-card (peek player1-deck)
+                      player2-card (peek player2-deck)
+                      round-winner (play-round player1-card player2-card)
+                      player1-new-deck (pop player1-deck)
+                      player2-new-deck (pop player2-deck)]
+                  (if (= round-winner 1)
+                    (recur (conj player1-new-deck player1-card player2-card) player2-new-deck)
+                    (recur player1-new-deck (conj player2-new-deck player2-card player1-card)))))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -2,18 +2,28 @@
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
-
-;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= 1 (play-round {:suit :spade :rank 4} {:suit :spade :rank 3})))
+    (is (= 2 (play-round {:suit :spade :rank 3} {:suit :spade :rank 4}))))
+  (testing "queens are higher rank than jacks"
+    (is (= 1 (play-round {:suit :spade :rank :queen} {:suit :spade :rank :jack}))))
+  (testing "kings are higher rank than queens"
+    (is (= 1 (play-round {:suit :spade :rank :king} {:suit :spade :rank :queen}))))
+  (testing "aces are higher rank than kings"
+    (is (= 1 (play-round {:suit :spade :rank :ace} {:suit :spade :rank :king}))))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= 1 (play-round {:suit :club :rank 2} {:suit :spade :rank 2}))))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= 1 (play-round {:suit :diamond :rank 2} {:suit :club :rank 2}))))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= 1 (play-round {:suit :heart :rank 2} {:suit :diamond :rank 2})))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
-
+  (testing "the player loses when they run out of cards"
+    (is (= 1 (play-game (list {:suit :spade :rank 2}) (list))))
+    (is (= 2 (play-game (list) (list {:suit :spade :rank 2})))))
+  (testing "main game loop terminates and reaches same winner regardless of player order"
+    (let [decks (split-at 26 (shuffle cards))]
+      (is (not (= (play-game (first decks) (second decks))
+                  (play-game (second decks) (first decks))))))))
